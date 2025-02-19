833d19a9e911e7bf64c06b334af70d3803784d8a
Syeda Nida Khader <60160145+syedanida@users.noreply.github.com>
2025-02-19 13:32:11 -0800

    Delete backend directory

--- backend/database.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
-
-db.serialize(() => {
-  db.run('CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, author TEXT)');
-  db.run('CREATE TABLE reviews (id INTEGER PRIMARY KEY, book_id INTEGER, review TEXT, FOREIGN KEY(book_id) REFERENCES books(id))');
-});
-
-module.exports = db;
